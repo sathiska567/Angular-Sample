61dcc2a7f6e6f4187adffb7453e74d155c8950b4
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-10-22 00:13:26 +0530

    updated

--- src/app/admin-feaches/admin-feaches.component.ts
+++ src/app/admin-feaches/admin-feaches.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrl: './admin-feaches.component.css'
 })
 export class AdminFeachesComponent {
-  
+   data = [
+    {
+      id: 1,
+      title: "Admin Dashboard",
+      description: "this is description",
+      subDescription:"hello"
+    },
+    {
+      id: 2,
+      title: "Admin Dashboard 1",
+      description: "this is description 1",
+      subDescription:"hello 1"
+    },
+    {
+      id: 3,
+      title: "Admin Dashboard 2",
+      description: "this is description 2",
+      subDescription:"hello 2"
+    },
+    {
+      id: 4,
+      title: "Admin Dashboard 3",
+      description: "this is description 3",
+      subDescription:"hello 3"
+    },
+    {
+      id: 5,
+      title: "Admin Dashboard 4",
+      description: "this is description 4",
+      subDescription:"hello 4"
+    },
+   ]
+
+  title = 'sample-project';
 }
